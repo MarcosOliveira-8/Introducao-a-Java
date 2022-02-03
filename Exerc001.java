70fc637f62bf815a2d1f390bf6bd23de5cfb2260
marcos de oliveira <marcosgithub@gmail.com>
2022-02-03 20:54:14 -0300

    Iniciando um serie de exercicio em java

---
+++ Exerc001.java
@@ -0,0 +1,27 @@
+/*
+Introdução à Programacão Java
+Exercicios retirados e adaptados para liguagem JAVA.
+Fonte: MENEZES,Nilo Ney Coutinho Introdução à Programação Python 3ª EDIÇÃO. Editora Novatec 2019.
+
+*/
+
+// Exercício 2.3 => Faça um programa que exiba seu nome.
+
+package Mundo_1;
+
+public class Exerc001 {
+
+        public static void main(String[] args) {
+
+            //Existem 2 formas de executar esse programa.
+            //1ª Forma:
+            System.out.println("Marcos");
+
+            //2ª Forma:
+            String nome = "Marcos";
+            System.out.println(nome);
+
+        }
+    }
+
+
